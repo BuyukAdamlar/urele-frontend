efb7dd21ebea61125bcf97f65999e7c7d7d81e77
Muhammed Yurga <muhammed_yurga@hotmail.com>
2022-12-25 14:33:42 +0300

    Add project files.

---
+++ urele.Client/Client/API.cs
@@ -0,0 +1,7 @@
+﻿namespace urele.Client
+{
+	public class API
+	{
+		public static string url { get; } = "https://apiurele.azurewebsites.net/";
+	}
+}
---
+++ urele.Client/Client/Program.cs
@@ -0,0 +1,20 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace urele.Client
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
+			builder.Services.AddBlazoredLocalStorage();
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(API.url) });
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
